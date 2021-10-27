bc7fe2a770dbda9d1e480d9383da1fc8e4243b0e
var1ch <varich96@gmail.com>
2021-10-27 17:05:07 +0300

    Initial Setup

---
+++ src/view/Root.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledRoot } from "./StyledRoot";
+
+export default class Root extends React.Component {
+  render() {
+    return (
+      <StyledRoot>
+        <h1>Hello</h1>
+      </StyledRoot>
+    );
+  }
+}
---
+++ src/view/StyledRoot.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledRoot = styled.div`
+  min-width: 720px;
+  max-width: 720px;
+  max-height: 100vh;
+  min-height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
